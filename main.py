7c391f7c5565383a17990bd1a521930974c94f06
alireza <rezaali1387@gmail.com>
2023-03-11 16:38:54 +0330

    add merge csv by element

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ import pandas as panda
 
 def print_hi(name):
     print(f"Hi {name}")
-    file = panda.read_csv("./datas/cy68j2_posts.csv")
+    coupons = panda.read_csv("./datas/coup.csv")
+    mobiles = panda.read_csv("./datas/numbers.csv")
+
+    out = panda.merge(coupons, mobiles, on=["created_date"])
+    print(out)
+    out.to_csv('./outs/out.csv')
+
+
+def get_coupons(path):
+    file = panda.read_csv(path)
     coupons = file.loc[file['post_type'] == 'shop_coupon']
 
     print(coupons)
